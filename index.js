3276dd9041caab944070a6ebb2cb25e51150e7ce
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-31 22:02:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var recipes = {
   quesadilla: ['tortilla', 'cheese'],
   omelette: ['eggs', 'cheese', 'peppers']
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
